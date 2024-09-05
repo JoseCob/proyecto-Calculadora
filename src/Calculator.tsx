e68561e30fab2322c9aac722c1cd5bb4a7a1c8e3
JoseCob <josecobkantun123@gmail.com>
2024-09-05 10:51:42 -0600

    Actualizacion del codigo
     se añadió nuevas instrucciones al código

--- src/Calculator.tsx
+++ src/Calculator.tsx
@@ -2,16 +2,58 @@ import { useState } from "react";
 import Button from './Button';
 
 function Calculator() {
-    const [displayValue, setDisplayValue ] = useState('0');
-    
-    return(
-        <div>
-            <div>{displayValue}</div>
-            {/* Aqui irán los botones de la calculadora */}
-            <Button value="1" onClick={() => setDisplayValue(displayValue + '1')}></Button>
-            <Button value="+" onClick={() => console.log('Logica para la suma')}></Button>
-        </div>
-    );
+    const [displayValue, setDisplayValue] = useState('0');
+    const [operator, setOperator] = useState<string | null>(null);
+    const [firstOperand, setFirstOperand] = useState<number | null>(null);
+
+    function handleNumberClick(value: string) {
+        setDisplayValue(displayValue === '0' ? value : displayValue + value);
+    }
+
+    function handleOperationClick(newOperator: string) {
+        if (firstOperand === null) {
+            setFirstOperand(parseFloat(displayValue));
+            setOperator(newOperator);
+            setDisplayValue('0');
+        } else {
+            calculate();
+            setOperator(newOperator);
+        }
+    }
+
+    function calculate() {
+        if (firstOperand !== null && operator !== null) {
+            const secondOperand = parseFloat(displayValue);
+            switch (operator) {
+                case '+':
+                    setDisplayValue((firstOperand + secondOperand).toString());
+                    break;
+                case '-':
+                    setDisplayValue((firstOperand - secondOperand).toString());
+                    break;
+                case '*':
+                    setDisplayValue((firstOperand * secondOperand).toString());
+                    break;
+                case '/':
+                    if (secondOperand === 0) {
+                        setDisplayValue('Error');
+                    } else {
+                        setDisplayValue((firstOperand / secondOperand).toString());
+                    }
+                    break;
+            }
+            setFirstOperand(null);
+            setOperator(null);
+        }
+    }
+
+    return (
+            <div>
+                <div>{displayValue}</div>
+                <Button value="1" onClick={() => handleNumberClick('1')} />{' '}
+                <Button value="+" onClick={() => handleOperationClick('+')} />{' '}
+            </div>
+        );
 }
 
 export default Calculator;
